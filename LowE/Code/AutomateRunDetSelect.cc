add41969209e58ddb134520a8b95d581160f1c96
Jamin Rager <jrager@pdsf7.nersc.gov>
2019-01-29 17:20:58 -0800

    updates to AutomateRunDetSelect.cc

--- LowE/Code/AutomateRunDetSelect.cc
+++ LowE/Code/AutomateRunDetSelect.cc
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <numeric>
 #include "TFile.h"
 #include "TROOT.h"
 #include "TChain.h"
@@ -23,16 +24,16 @@
 
 using namespace std;
 
-void MakeZScores();
-//void PlotZScores();
+void Method1();
+void Method2();
 
 int main()
 {
-    MakeZScores();
-    //PlotZScores();
+    //Method1();
+    Method2();
 }
 
-void MakeZScores()
+void Method1()
 {
     // Load input files
     string inFile1 = "~/LowE/Data/BlindRunSelectDS5b_5kThresh.root";
@@ -50,29 +51,75 @@ void MakeZScores()
     TH1F *h2 = (TH1F*)f2->Get("det");
     
     // Get stats from plots
-    Double_t meanRun = h1->GetMean(Int_t axis = 2);
-    Double_t stdevRun = h1->GetStdDev(Int_t axis = 2);
-    Double_t meanDet = h2->GetMean(Int_t axis = 2);
-    Double_t stdevDet = h2->GetStdDev(Int_t axis = 2);
+    Int_t axis;
+    Double_t meanRun = h1->GetMean(axis = 2);
+    Double_t stdevRun = h1->GetStdDev(axis = 2);
+    Double_t meanDet = h2->GetMean(axis = 2);
+    Double_t stdevDet = h2->GetStdDev(axis = 2);
     
     cout << "meanRun: " << meanRun << " stdevRun: " << stdevRun << " meanDet: " << meanDet << " stdevDet: " << stdevDet << endl;
     
+    Int_t run;
+
     //DS5b: i=0; i<1565
     for (Int_t i=7; i<1565; i++)
     {
+        run = i+22393;
         Double_t ctsRun = h1->Integral(i,i);
-        Doublt_t zRun = (ctsRun - meanRun)/stdevRun;
+        Double_t zRun = (ctsRun - meanRun)/stdevRun;
         if (zRun>=3)
         {
             for (Int_t j=584; j<1331; j++)
             {
                 Double_t ctsDet = h2->Integral(j,j);
-                Doublt_t zDet = (ctsDet - meanDet)/stdevDet;
+                Double_t zDet = (ctsDet - meanDet)/stdevDet;
                 if (zDet>=3)
                 {
-                    cout << "run: " << run << " zRun: " << zRun << " channel: " << channel << " zDet: " zDet << endl;
+		   cout << "run: " << run << " zRun: " << zRun << " channel: " << j << " zDet: " << zDet << endl;
                 }
             }
         }
     }
+}
+
+void Method2()
+{
+    // Load input files
+    string inFile1 = "~/LowE/Data/BlindRunSelectDS5b_5kThresh.root";
+    TFile *f1 = new TFile(inFile1.c_str());
+    //string inFile2 = "~/LowE/AnalysisPlots/latest/DetSelectDS5b_5kThresh.root";
+    //TFile *f2 = new TFile(inFile2.c_str());
+
+    // Load TH2's
+    TH1F *h1 = (TH1F*)f1->Get("run");
+    //TH1F *h2 = (TH1F*)f2->Get("det");
+
+    // Initialize vectors
+    std::vector<double> *runCts = 0;
+    //std::vector<double> *chan = 0;
+
+    //Run loop
+    //DS5b: i=0; i<1565
+    for (Int_t i=7; i<1565; i++)
+      {
+        double ctsRun = h1->Integral(i,i);
+	runCts->push_back(ctsRun);
+      }
+
+    //Channel loop
+    //DS5b: j=584; j<1331;
+    //for (Int_t j=584; j<1331; j++)
+      //{
+        //double ctsDet = h2->Integral(j,j);
+	//chan->push_back(ctsDet);
+      //}
+
+    double sum = std::accumulate((*runCts).begin(), (*runCts).end(), 0.0);
+    double mean = sum / (*runCts).size();
+    double sq_sum = std::inner_product((*runCts).begin(), (*runCts).end(), (*runCts).begin(), 0.0);
+    double stdev = std::sqrt(sq_sum / (*runCts).size() - mean * mean);
+
+    cout << "sum " << sum << " mean " << mean << " sq_sum " << sq_sum << "stdev" << stdev << endl;
+
+    cout << "done" <<  endl;
 }

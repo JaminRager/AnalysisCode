c8866f056d6f0a39387b05f5cbe88941e5849b6d
Jamin Rager <jrager@pdsf7.nersc.gov>
2019-01-30 16:26:23 -0800

    rewrote AutomateRunDetSelect.cc

--- LowE/Code/AutomateRunDetSelect.cc
+++ LowE/Code/AutomateRunDetSelect.cc
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <fstream>
 #include <numeric>
+#include <vector>
 #include "TFile.h"
 #include "TROOT.h"
 #include "TChain.h"
@@ -24,102 +25,75 @@
 
 using namespace std;
 
-void Method1();
-void Method2();
-
 int main()
-{
-    //Method1();
-    Method2();
-}
-
-void Method1()
 {
     // Load input files
     string inFile1 = "~/LowE/Data/BlindRunSelectDS5b_5kThresh.root";
     TFile *f1 = new TFile(inFile1.c_str());
-    string inFile2 = "~/LowE/AnalysisPlots/latest/DetSelectDS5b_5kThresh.root";
+    string inFile2 = "~/LowE/AnalysisPlots/latest/DetectorSelection/DetSelectDS5b_5kThresh.root";
     TFile *f2 = new TFile(inFile2.c_str());
-    
-    // Create output file
-    //string outFile = "/global/u1/j/jrager/LowE/Data/ZScores.txt";
-    //TFile *f3 = new TFile(outFile.c_str(),"RECREATE");
-    //cout << "Creating outpout file" <<  endl;
-    
+
     // Load TH2's
     TH1F *h1 = (TH1F*)f1->Get("run");
     TH1F *h2 = (TH1F*)f2->Get("det");
-    
-    // Get stats from plots
-    Int_t axis;
-    Double_t meanRun = h1->GetMean(axis = 2);
-    Double_t stdevRun = h1->GetStdDev(axis = 2);
-    Double_t meanDet = h2->GetMean(axis = 2);
-    Double_t stdevDet = h2->GetStdDev(axis = 2);
-    
-    cout << "meanRun: " << meanRun << " stdevRun: " << stdevRun << " meanDet: " << meanDet << " stdevDet: " << stdevDet << endl;
-    
-    Int_t run;
-
-    //DS5b: i=0; i<1565
-    for (Int_t i=7; i<1565; i++)
-    {
-        run = i+22393;
-        Double_t ctsRun = h1->Integral(i,i);
-        Double_t zRun = (ctsRun - meanRun)/stdevRun;
-        if (zRun>=3)
-        {
-            for (Int_t j=584; j<1331; j++)
-            {
-                Double_t ctsDet = h2->Integral(j,j);
-                Double_t zDet = (ctsDet - meanDet)/stdevDet;
-                if (zDet>=3)
-                {
-		   cout << "run: " << run << " zRun: " << zRun << " channel: " << j << " zDet: " << zDet << endl;
-                }
-            }
-        }
-    }
-}
-
-void Method2()
-{
-    // Load input files
-    string inFile1 = "~/LowE/Data/BlindRunSelectDS5b_5kThresh.root";
-    TFile *f1 = new TFile(inFile1.c_str());
-    //string inFile2 = "~/LowE/AnalysisPlots/latest/DetSelectDS5b_5kThresh.root";
-    //TFile *f2 = new TFile(inFile2.c_str());
-
-    // Load TH2's
-    TH1F *h1 = (TH1F*)f1->Get("run");
-    //TH1F *h2 = (TH1F*)f2->Get("det");
 
     // Initialize vectors
-    std::vector<double> *runCts = 0;
-    //std::vector<double> *chan = 0;
+    std::vector<double> run;
+    std::vector<double> chan;
 
     //Run loop
     //DS5b: i=0; i<1565
     for (Int_t i=7; i<1565; i++)
       {
         double ctsRun = h1->Integral(i,i);
-	runCts->push_back(ctsRun);
+	run.push_back(ctsRun);
+	//cout << "i " << i << " ctsRun " << ctsRun << endl;
       }
+
+    double sumR = std::accumulate(run.begin(), run.end(), 0.0);
+    double meanR = sumR / run.size();
+    double sq_sumR = std::inner_product(run.begin(), run.end(), run.begin(), 0.0);
+    double stdevR = std::sqrt(sq_sumR / run.size() - meanR * meanR);
+    cout << "sumR " << sumR << " meanR " << meanR << " sq_sumR " << sq_sumR << " stdevR " << stdevR << endl;
 
     //Channel loop
     //DS5b: j=584; j<1331;
-    //for (Int_t j=584; j<1331; j++)
-      //{
-        //double ctsDet = h2->Integral(j,j);
-	//chan->push_back(ctsDet);
-      //}
+    for (Int_t j=584; j<1331; j++)
+      {
+        double ctsDet = h2->Integral(j,j);
+        chan.push_back(ctsDet);
+      }
 
-    double sum = std::accumulate((*runCts).begin(), (*runCts).end(), 0.0);
-    double mean = sum / (*runCts).size();
-    double sq_sum = std::inner_product((*runCts).begin(), (*runCts).end(), (*runCts).begin(), 0.0);
-    double stdev = std::sqrt(sq_sum / (*runCts).size() - mean * mean);
-
-    cout << "sum " << sum << " mean " << mean << " sq_sum " << sq_sum << "stdev" << stdev << endl;
+    double sumC = std::accumulate(chan.begin(), chan.end(), 0.0);
+    double meanC = sumC / chan.size();
+    double sq_sumC = std::inner_product(chan.begin(), chan.end(), chan.begin(), 0.0);
+    double stdevC = std::sqrt(sq_sumC / chan.size() - meanC * meanC);
+    cout << "sumC " << sumC << " meanC " << meanC << " sq_sumC " << sq_sumC << " stdevC " << stdevC << endl;
+    
+    //seems to be skipping this part of the program. maybe if I try looping over vectors run and chan instead?
+    //would have to change ranges on loops to go through entier range of run and channel in data files
+    //DS5b: i=0; i<1565
+    for (Int_t k=7; k<1565; k++)
+      {
+        Int_t r = k+22393;
+        double ctsRun = run[k];
+        double zRun = (ctsRun - meanR)/stdevR;
+        if (zRun>=3)
+	  {
+            for (Int_t l=584; l<1331; l++)
+	      {
+                double ctsDet = chan[l];
+                double zDet = (ctsDet - meanC)/stdevC;
+                if (zDet>=3)
+		  {
+		    cout << "run: " << r << " zRun: " << zRun << " channel: " << l << " zDet: " << zDet << endl;
+		  }
+	      }
+	  }
+      }
+    
 
     cout << "done" <<  endl;
+
+    return 0;
 }

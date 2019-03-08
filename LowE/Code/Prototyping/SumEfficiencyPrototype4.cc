99aa98f0231fa89a6bfa89be992519aac1970752
Jamin Rager <jrager@pdsf8.nersc.gov>
2019-03-08 08:16:09 -0800

    added SumEfficiencyPrototype4.cc

--- LowE/Code/Prototyping/SumEfficiencyPrototype4.cc
+++ LowE/Code/Prototyping/SumEfficiencyPrototype4.cc
@@ -49,8 +49,7 @@ std::map<int, double> det_mass_err;
 std::map<int, double> det_lt;
 std::map<int, TFile*> f;
 std::map<int, TGraphAsymmErrors*> geff;
-
- cout << "detector mass and livetime maps created" << endl;
+cout << "detector mass and livetime maps created" << endl;
 
 // Variables to assign to info file columns
 int idx, ds, chan, cpd;
@@ -61,12 +60,13 @@ int n,m;
 string path;
 char buffer1[50];
 char buffer2[50];
+cout << "map variables initialized" << endl;
     
 ifstream infile(info_file);
 while (infile >> idx >> ds >> cpd >> chan >> serial_num >> mass >> mass_err >> lt)
 {
-    det_mass[idx] = mass;
-    det_mass_err[idx] = mass_err
+    det_mass[idx] = mass/1000;
+    det_mass_err[idx] = mass_err/1000;
     det_lt[idx] = lt;
     n = sprintf(buffer1,"%i",ds);
     m = sprintf(buffer2,"%i",chan);
@@ -75,39 +75,51 @@ while (infile >> idx >> ds >> cpd >> chan >> serial_num >> mass >> mass_err >> l
     geff[idx] = (TGraphAsymmErrors*)f[idx]->Get("Graph");
 }
 cout << "mass, livetime, TFile, and TGraph maps filled" << endl;
-    
-double *eff[15][20] = {{}};
-double *errHi[15][20] = {{}};
-double *errLow[15][20] = {{}};
+
+// chop these up into smaller arrays!
+double eff1[14][20] = {{}};
+double errHi1[14][20] = {{}};
+double errLow1[14][20] = {{}};
 double effSum[20] = {};
-double sumErrHi[20] = {};
-double sumSumLow[20] = {};
+double errSumHi[20] = {};
+double errSumLow[20] = {};
+double errSumHiSqrt[20] = {};
+double errSumLowSqrt[20] = {};
 double x[20] = {};
 double xerr[20] = {};
 cout << "arrays initialized" << endl;
 
+double a = 0;
+double b = 0;
+double c = 0;
+cout << "initialized placeholders" << endl;
+
 for(int i = 0; i < 20; i++){
-    for(int j = 0; j < 15; j++){
-        (*eff)[j][i] = geff[j]->Eval(i*5);
-        cout << "eff[j][i], " << j << ", " << i << ", " << (*eff)[j][i] << endl;
-        effSum[i] = effSum[i] + ((*eff)[j][i] * det_mass[j] * det_lt[j]);
-        sumErrHi[i] = sqrt(sumErrHi[i] + (((*errHi)[j][i]*det_mass[j]*det_lt[j])*((*errHi)[j][i]*det_mass[j]*det_lt[j])) + ((det_mass_err[j]*(*eff)[j][i]*det_lt[j])*(det_mass_err[j]*(*eff)[j][i]*det_lt[j])));
-        sumErrLow[i] = sqrt(sumErrLow[i] + (((*errLow)[j][i]*det_mass[j]*det_lt[j])*((*errLow)[j][i]*det_mass[j]*det_lt[j])) + ((det_mass_err[j]*(*eff)[j][i]*det_lt[j])*(det_mass_err[j]*(*eff)[j][i]*det_lt[j])));
+    for(int j = 0; j < 14; j++){
+        eff[j][i] = geff[j]->Eval(i*5);
+	errHi[j][i] = geff[j]->GetErrorYhigh(i);
+	errLow[j][i] = geff[j]->GetErrorYlow(i);
+        cout << "eff[j][i], errHi[j][i], errLow[j][i], " << j << ", " << i << ", " << eff[j][i]  << " " << errHi[j][i] << " " << errLow[j][i] << endl;
+        effSum[i] = effSum[i] + (eff[j][i]*det_mass[j]*det_lt[j]);
+        errSumHi[i] = errSumHi[i] + ((errHi[j][i]*det_mass[j]*det_lt[j])*(errHi[j][i]*det_mass[j]*det_lt[j])) + ((det_mass_err[j]*eff[j][i]*det_lt[j])*(det_mass_err[j]*eff[j][i]*det_lt[j]));
+        errSumLow[i] = errSumLow[i] + ((errLow[j][i]*det_mass[j]*det_lt[j])*(errLow[j][i]*det_mass[j]*det_lt[j])) + ((det_mass_err[j]*eff[j][i]*det_lt[j])*(det_mass_err[j]*eff[j][i]*det_lt[j]));
 	
     }
+    errSumHiSqrt[i] = sqrt(errSumHi[i]);
+    errSumLowSqrt[i] = sqrt(errSumLow[i]);
     x[i] = i*5;
     cout << "x[i]" << x[i] << endl;
     xerr[i] = 0;
 }
 cout << "Loop through data points and map indices" << endl;
 
-string outFile = "~/LowE/Code/Prototyping/CombinedEffPrototype3.root";
+string outFile = "~/LowE/Code/Prototyping/Prototype2.root";
 TFile *f0 = new TFile(outFile.c_str(),"recreate");
 cout << "Creating outpout file" <<  endl;
 f0->cd();
 
 cout << "Creating new TGraph" << endl;
-TGraphAsymmErrors *geffSum = new TGraphAsymmErrors(20, x, effSum, xerr, xerr, effErr, effErr);
+TGraphAsymmErrors *geffSum = new TGraphAsymmErrors(20, x, effSum, xerr, xerr, errSumLowSqrt, errSumHiSqrt);
 geffSum->SetMarkerStyle(21);
 geffSum->SetMarkerColor(kRed);
 geffSum->SetTitle("DS1 efficiency weighted exposure");

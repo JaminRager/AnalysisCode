6d2e173b2669a946411405ab207b4aee082a6126
Jamin Rager <ragerj@unc.edu>
2019-04-09 13:42:05 -0400

    updated SumEffNormExpanded.cc

--- LowE/Code/SumEffNormExpanded.cc
+++ LowE/Code/SumEffNormExpanded.cc
@@ -40,7 +40,7 @@ int main()
 {
 
 // Info file
-string info_file = "det_info_lowE.txt";
+string info_file = "det_info_lowE_expanded.txt";
 cout << "det_info file loaded" << endl;
 
 // maps of detector mass, detector livetime, detector efficiency TFile, detector efficiency TGraph
@@ -79,9 +79,9 @@ while (infile >> idx >> ds >> cpd >> chan >> serial_num >> mass >> mass_err >> l
 cout << "mass, livetime, TFile, and TGraph maps filled" << endl;
 
 // chop these up into smaller arrays!
-double eff[158][20] = {{}};
-double errHi[158][20] = {{}};
-double errLow[158][20] = {{}};
+double eff[115][20] = {{}};
+double errHi[115][20] = {{}};
+double errLow[115][20] = {{}};
 double effSum[20] = {};
 double errSumHi[20] = {};
 double errSumLow[20] = {};
@@ -97,20 +97,21 @@ cout << "arrays initialized" << endl;
 cout << "initialized placeholders" << endl;
 
 for(int i = 0; i < 20; i++){
-    for(int j = 0; j < 158; j++){
-        eff[j][i] = geff[j]->Eval(i*5);
+    for(int j = 0; j < 115; j++){
+    eff[j][i] = geff[j]->Eval(i*5);
 	errHi[j][i] = geff[j]->GetErrorYhigh(i);
 	errLow[j][i] = geff[j]->GetErrorYlow(i);
         cout << "eff[j][i], errHi[j][i], errLow[j][i], " << j << ", " << i << ", " << eff[j][i]  << " " << errHi[j][i] << " " << errLow[j][i] << endl;
-        effSum[i] = effSum[i] + (eff[j][i]*det_mass[j]*det_lt[j]);
-        //errSumHi[i] = errSumHi[i] + ((errHi[j][i]*det_mass[j]*det_lt[j])*(errHi[j][i]*det_mass[j]*det_lt[j])) + ((det_mass_err[j]*eff[j][i]*det_lt[j])*(det_mass_err[j]*eff[j][i]*det_lt[j]));
-	errSumHi[i] = errSumHi[i] + pow(errHi[j][i],2)*pow(...
-							   //errSumLow[i] = errSumLow[i] + ((errLow[j][i]*det_mass[j]*det_lt[j])*(errLow[j][i]*det_mass[j]*det_lt[j])) + ((det_mass_err[j]*eff[j][i]*det_lt[j])*(det_mass_err[j]*eff[j][i]*det_lt[j]));
+    effSum[i] = effSum[i] + (eff[j][i]*det_mass[j]*det_lt[j]);
+    //errSumHi[i] = errSumHi[i] + ((errHi[j][i]*det_mass[j]*det_lt[j])*(errHi[j][i]*det_mass[j]*det_lt[j])) + ((det_mass_err[j]*eff[j][i]*det_lt[j])*(det_mass_err[j]*eff[j][i]*det_lt[j]));
+	errSumHi[i] = errSumHi[i] + pow((errHi[j][i]*det_mass[j]*det_lt[j])/3454,2) + pow((det_exp_err[i]*eff[j][i])/3454,2)
+    //errSumLow[i] = errSumLow[i] + ((errLow[j][i]*det_mass[j]*det_lt[j])*(errLow[j][i]*det_mass[j]*det_lt[j])) + ((det_mass_err[j]*eff[j][i]*det_lt[j])*(det_mass_err[j]*eff[j][i]*det_lt[j]));
+    errSumLow[i] = errSumLow[i] + pow((errLow[j][i]*det_mass[j]*det_lt[j])/3454,2) + pow((det_exp_err[i]*eff[j][i])/3454,2)
 	
     }
-    effSum[i] = effSum[i]/4378.6;
-    errSumHiSqrt[i] = (sqrt(errSumHi[i]))/4378.6;
-    errSumLowSqrt[i] = (sqrt(errSumLow[i]))/4378.6;
+    effSum[i] = effSum[i]/3454;
+    errSumHiSqrt[i] = (sqrt(errSumHi[i] + pow((effSum[i]*21),2)/pow(3454,4)));
+    errSumLowSqrt[i] = (sqrt(errSumLow[i] + pow((effSum[i]*21),2)/pow(3454,4)));
     x[i] = i*5;
     cout << "x[i]" << x[i] << endl;
     xerr[i] = 0;
@@ -118,7 +119,7 @@ for(int i = 0; i < 20; i++){
 cout << "Loop through data points and map indices" << endl;
 
 //string outFile = "~/LowE/Code/Data/CombinedEfficiencies/NormalizedEfficiencyDS1_6.root";
-string outFile = "testEff.root";
+string outFile = "test3.root";
 TFile *f0 = new TFile(outFile.c_str(),"recreate");
 cout << "Creating outpout file" <<  endl;
 f0->cd();
@@ -128,8 +129,8 @@ TGraphAsymmErrors *geffSum = new TGraphAsymmErrors(20, x, effSum, xerr, xerr, er
 geffSum->SetMarkerStyle(21);
 geffSum->SetMarkerColor(kRed);
 geffSum->SetTitle("DS1-6 efficiency weighted exposure");
-geffSum->GetXaxis()->SetTitle("trapENFCal (keV)");
-geffSum->GetYaxis()->SetTitle("eff wt expos (kg*d)");
+geffSum->GetXaxis()->SetTitle("Energy (keV)");
+geffSum->GetYaxis()->SetTitle("efficiency");
 geffSum->Write();
 f0->Close();
 
